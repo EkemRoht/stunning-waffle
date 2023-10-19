9babb2eaf890c6bb32b4aebb2be771394fe3cbac
EkemRoht <ekemroht@gmail.com>
2023-10-20 02:27:47 +0500

    tables

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,5 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-<input type="checkbox" role="switch" />
+<script>
+    import DiceTable from "./DiceTable.svelte";
+</script>
+
+<DiceTable/>
---
+++ src/routes/DiceTable.svelte
@@ -0,0 +1,48 @@
+<script>
+    let rows = 2; // Начальное количество строк
+    let cols = 2; // Начальное количество столбцов
+    let tableData = Array.from({ length: rows }, () => Array(cols).fill(''));
+
+    function addRow() {
+        rows++;
+        tableData.push(Array(cols).fill(''));
+    }
+
+    function removeRow() {
+        if (rows > 2) {
+            rows--;
+            tableData.pop();
+        }
+    }
+
+    function addColumn() {
+        cols++;
+        tableData.forEach(row => row.push(''));
+    }
+
+    function removeColumn() {
+        if (cols > 2) {
+            cols--;
+            tableData.forEach(row => row.pop());
+        }
+    }
+</script>
+
+<div class="data-table">
+    <button on:click={addRow}>Добавить строку</button>
+    <button on:click={removeRow}>Удалить строку</button>
+    <button on:click={addColumn}>Добавить столбец</button>
+    <button on:click={removeColumn}>Удалить столбец</button>
+
+    <table>
+        {#each tableData as row, rowIndex}
+            <tr>
+                {#each row as cell, colIndex}
+                    <td>
+                        <input type="text" bind:value={tableData[rowIndex][colIndex]}>
+                    </td>
+                {/each}
+            </tr>
+        {/each}
+    </table>
+</div>
