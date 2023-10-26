33887e8467c2285f8f2066b8a731bc0769a02627
EkemRoht <ekemroht@gmail.com>
2023-10-27 02:59:29 +0500

    json uploading

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,31 @@
+<!-- App.svelte -->
 <script>
-    import NewNPC from "./NewNPC.svelte";
+    let selectedFiles = [];
+    let jsonData = {};
+
+    async function handleFileInput(event) {
+        selectedFiles = event.target.files;
+    }
+
+    async function loadJsonFiles() {
+        jsonData = {}; // Очищаем объект перед загрузкой новых файлов
+
+        for (let i = 0; i < selectedFiles.length; i++) {
+            const file = selectedFiles[i];
+            if (file.type === 'application/json') {
+                try {
+                    const data = await file.text();
+                    jsonData[file.name] = JSON.parse(data);
+                } catch (error) {
+                    console.error(`Ошибка при обработке файла ${file.name}:`, error);
+                }
+            }
+        }
+
+        // Вывести данные в консоль
+        console.log('Загруженные JSON файлы:', jsonData);
+    }
 </script>
 
-<NewNPC/>
+<input type="file" multiple on:change={handleFileInput} />
+<button on:click={loadJsonFiles}>Загрузить и вывести JSON файлы</button>
