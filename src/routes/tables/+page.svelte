6a8b6b6e19021fbf2d6c723228e694e8be7d471c
EkemRoht <ekemroht@gmail.com>
2023-10-26 05:32:53 +0500

    tables list and creating page proto

--- src/routes/tables/+page.svelte
+++ src/routes/tables/+page.svelte
@@ -1,44 +1,9 @@
 <script>
     import {db} from "$lib/js/db.js";
 
-    let status = "";
+    import TableList from "./TableList.svelte";
+    import TableEditor from "./TableEditor.svelte";
 
-    let moduleName = "";
-    let moduleData = "";
-    async function addModule() {
-        try {
-            const id = await db.modules.add({
-                name: moduleName,
-                content: moduleData
-            });
-
-            status = `Модуль ${moduleName} был сохранёнс с айди ${id}`;
-            moduleName = "";
-            moduleData = [];
-        } catch (err) {
-            status = `Модуль ${moduleName} не удалось сохранить. Ошибка: ${err}`;
-        }
-    }
 </script>
-
-<div>
-    <p>{status}</p>
-    <fieldset>
-        <legend>Add new module</legend>
-        <label>
-            Name:
-            <input
-                    type="text"
-                    bind:value={moduleName} />
-        </label>
-        <br/>
-        <label>
-            Age:
-            <input
-                    type="text"
-                    bind:value={moduleData} />
-        </label>
-        <br />
-        <button on:click={addModule}>Add Friend</button>
-    </fieldset>
-</div>
+<TableList/>
+<TableEditor/>
---
+++ src/routes/tables/TableList.svelte
@@ -0,0 +1,16 @@
+<script>
+    import { liveQuery } from "dexie";
+    import {db} from "$lib/js/db.js";
+
+    let modules = liveQuery(
+        () => db.modules.toArray()
+    );
+
+</script>
+<ul>
+    {#if $modules}
+        {#each $modules as module (module.id)}
+            <li>{module.name}</li>
+        {/each}
+    {/if}
+</ul>
